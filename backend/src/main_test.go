95a0dfc0923706e58eb17ada5f82947f4ce73374
Edwin Barreto <ebarreto@telesign.com>
2024-10-02 17:51:46 -0500

    more test added

--- backend/src/main_test.go
+++ backend/src/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "bytes"
     "net/http"
     "net/http/httptest"
     "testing"
@@ -18,22 +19,54 @@ func TestHomeHandler(t *testing.T) {
     handler.ServeHTTP(rr, req)
 
     if status := rr.Code; status != http.StatusOK {
-        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+        t.Errorf("handler devolvió status incorrecto: obtuvo %v esperaba %v", status, http.StatusOK)
+    }
+
+    expected := `{"message": "Welcome to the Backend API!"}`
+    if rr.Body.String() != expected {
+        t.Errorf("handler devolvió respuesta inesperada: obtuvo %v esperaba %v", rr.Body.String(), expected)
     }
 }
 
-func TestNotFoundHandler(t *testing.T) {
-    req, err := http.NewRequest("GET", "/non-existing", nil)
+func TestContactHandler(t *testing.T) {
+    contactJSON := []byte(`{"name":"John Doe", "email":"john@example.com", "message":"Hello!"}`)
+    req, err := http.NewRequest("POST", "/api/contact", bytes.NewBuffer(contactJSON))
     if err != nil {
         t.Fatal(err)
     }
 
     rr := httptest.NewRecorder()
-    handler := http.HandlerFunc(HomeHandler)
+    handler := http.HandlerFunc(contactHandler)
+
+    handler.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler devolvió status incorrecto: obtuvo %v esperaba %v", status, http.StatusOK)
+    }
+
+    expected := `{"message":"Thank you, John Doe. Your message has been received."}`
+    if rr.Body.String() != expected {
+        t.Errorf("handler devolvió respuesta inesperada: obtuvo %v esperaba %v", rr.Body.String(), expected)
+    }
+}
+
+func TestNotFoundHandler(t *testing.T) {
+    req, err := http.NewRequest("GET", "/not-found", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(notFoundHandler)
 
     handler.ServeHTTP(rr, req)
 
     if status := rr.Code; status != http.StatusNotFound {
-        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+        t.Errorf("handler devolvió status incorrecto: obtuvo %v esperaba %v", status, http.StatusNotFound)
+    }
+
+    expected := "Not found\n"
+    if rr.Body.String() != expected {
+        t.Errorf("handler devolvió respuesta inesperada: obtuvo %v esperaba %v", rr.Body.String(), expected)
     }
 }
