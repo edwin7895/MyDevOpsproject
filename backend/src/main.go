6e1eb72c1b6e2481cf58469e6bde79ab34fe9b15
Edwin Barreto <ebarreto@telesign.com>
2024-10-02 17:03:14 -0500

    fucntion name updated

--- backend/src/main.go
+++ backend/src/main.go
@@ -32,7 +32,7 @@ func main() {
     }
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
     enableCors(w)
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
